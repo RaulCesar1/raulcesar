4b42cae89079a413afd32d784c34d6cd59e73f75
Raul César <raulcesardeoliveira2@gmail.com>
2021-05-24 06:23:41 -0300

    Tema do site remodelado, dark mode (fundo preto, texto branco)

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,23 @@ import Head from 'next/head';
 
 export default function Home() {
     return (
-    <body className={`bg-gray-900 overflow-hidden select-none font-mono`}>
-      <Head>
-        <title>Raul C. Oliveira</title>
-      </Head>
+      <body className="overflow-hidden select-none font-mono bg-black">
+        <Head>
+          <title>Raul C. Oliveira</title>
+        </Head>
 
-      <div className={`text-yellow-300 flex h-screen justify-center items-center`}>
-        <div className="text-center">
-          <h1 className="text-grande">Raul César</h1>
-          <div className="text-medio">
-            <a className={`inline hover:text-indigo-600`} target="_blank" href="https://github.com/RaulCesar1">Github&nbsp;&nbsp;</a>
-            <a className={`inline hover:text-indigo-600`} target="_blank" href="https://www.npmjs.com/~raulcesaroliveira">NPM&nbsp;&nbsp;</a>
-            <a className={`inline hover:text-indigo-600`} target="_blank" href="https://twitter.com/RaulCsrOliveira">Twitter&nbsp;&nbsp;</a>
-            <a className={`inline hover:text-indigo-600`} target="_blank" href="https://www.youtube.com/channel/UCX0TjriOFMsEHv0UDS5eIEQ">Youtube</a>
+        <div className="flex h-screen justify-center items-center text-white">
+          <div className="text-center">
+            <h1 className="text-grande">Raul César</h1>
+            <div className="text-medio space-x-5">
+              <a className="inline hover:text-indigo-600" target="_blank" href="https://github.com/RaulCesar1">Github</a>
+              <a className="inline hover:text-indigo-600" target="_blank" href="https://www.npmjs.com/~raulcesaroliveira">NPM</a>
+              <a className="inline hover:text-indigo-600" target="_blank" href="https://twitter.com/RaulCsrOliveira">Twitter</a>
+              <a className="inline hover:text-indigo-600" target="_blank" href="https://www.youtube.com/channel/UCX0TjriOFMsEHv0UDS5eIEQ">Youtube</a>
+            </div>
           </div>
         </div>
-      </div>
 
-    </body>
+      </body>
   )
 }
